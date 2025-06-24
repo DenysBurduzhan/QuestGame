1d73292a23b8fbe99c7615d9e5428137d1e9ab09
Denys <denisburduzan@gmail.com>
2025-06-25 01:43:39 +0300

    added tests ana changed plot

--- Oleksandr-JR-Example/src/main/java/controller/QuizServlet.java
+++ Oleksandr-JR-Example/src/main/java/controller/QuizServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 
 @WebServlet(name = "QuizServlet", value = "/quiz")
 public class QuizServlet extends HttpServlet {
--- Oleksandr-JR-Example/src/main/java/repository/QuestionRepository.java
+++ Oleksandr-JR-Example/src/main/java/repository/QuestionRepository.java
@@ -10,52 +10,63 @@ public class QuestionRepository {
 
     private final ArrayList<Question> questions = new ArrayList<>();
 
-
     private QuestionRepository() {
         questions.add(Question.builder()
-                .text("Ви втрачаєте пам'ять. Прийняти виклик НЛО?")
-                .answers(new ArrayList<>(List.of("Прийняти виклик", "Відхилити виклик")))
-                .nextQuestions(new ArrayList<>(List.of(1, 5)))
+                .text("Вам пропонують почати дослідження всесвіту. Прийняти пропозицію?")
+                .answers(new ArrayList<>(List.of("Прийняти", "Відхилити")))
+                .nextQuestions(new ArrayList<>(List.of(1, 2)))
                 .build());
 
         questions.add(Question.builder()
-                .text("Ви прийняли виклик. Піднятися на капітанський місток?")
-                .answers(new ArrayList<>(List.of("Піднятися на місток", "Відмовитися")))
-                .nextQuestions(new ArrayList<>(List.of(2, 4)))
+                .text("Штовхнути темну матерію?")
+                .answers(new ArrayList<>(List.of("Так", "Ні")))
+                .nextQuestions(new ArrayList<>(List.of(3, 5)))
                 .build());
 
         questions.add(Question.builder()
-                .text("Ви піднялися на місток. Хто ви?")
-                .answers(new ArrayList<>(List.of("Розповісти правду", "Збрехати")))
-                .nextQuestions(new ArrayList<>(List.of(6, 7)))
-                .build());
-
-        questions.add(Question.builder()
-                .text("Якийсь інший варіант?")
-                .answers(new ArrayList<>(List.of("Тест1", "Тест2")))
-                .nextQuestions(new ArrayList<>(List.of(0, 0)))
-                .build());
-
-        questions.add(Question.builder()
-                .text("Ви не пішли на переговори. Поразка.")
+                .text("У результаті вашого відказу, дослідження було перенесене на великий термін. Поразка")
                 .answers(new ArrayList<>())
                 .nextQuestions(new ArrayList<>())
                 .build());
 
         questions.add(Question.builder()
-                .text("Ви відхилили виклик. Поразка.")
+                .text("Випарувалася невелика чорна діра, та залишився новий елемент під назвою — частка Бога! Що робимо далі?")
+                .answers(new ArrayList<>(List.of("Збільшити навантаження!", "Дослідити частку")))
+                .nextQuestions(new ArrayList<>(List.of(4, 9)))
+                .build());
+
+        questions.add(Question.builder()
+                .text("Чорна діра вбила все живе! Поразка.")
                 .answers(new ArrayList<>())
                 .nextQuestions(new ArrayList<>())
                 .build());
 
         questions.add(Question.builder()
-                .text("Вас повернули додому. Перемога!")
+                .text("Що будете робити?")
+                .answers(new ArrayList<>(List.of("Розвинути проект світлої матерії", "До дому.")))
+                .nextQuestions(new ArrayList<>(List.of(6, 2)))
+                .build());
+
+        questions.add(Question.builder()
+                .text("Скільки матерії додати до коллайдеру?")
+                .answers(new ArrayList<>(List.of("6 ммоль", "100 ммоль")))
+                .nextQuestions(new ArrayList<>(List.of(7, 8)))
+                .build());
+
+        questions.add(Question.builder()
+                .text("Людство позбулося енергетичної кризи. Перемога!")
                 .answers(new ArrayList<>())
                 .nextQuestions(new ArrayList<>())
                 .build());
 
         questions.add(Question.builder()
-                .text("Ваша брехня була викрита. Поразка.")
+                .text("Нейтронна бомба вбила все живе! Поразка!")
+                .answers(new ArrayList<>())
+                .nextQuestions(new ArrayList<>())
+                .build());
+
+        questions.add(Question.builder()
+                .text("Людство позбулося енергетичної кризи. Перемога!")
                 .answers(new ArrayList<>())
                 .nextQuestions(new ArrayList<>())
                 .build());
@@ -67,13 +78,9 @@ public class QuestionRepository {
         }
 
         return INSTANCE;
-
     }
-
 
     public ArrayList<Question> getAll() {
         return questions;
     }
-
-
 }
